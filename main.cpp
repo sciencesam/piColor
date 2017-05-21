fe204b961910f9a0c98025317b43f5bf3cf2d18a
Sam Stone <samstone@Sams-MBP.fios-router.home>
2017-05-21 15:35:57 -0400

    tes

--- main.cpp
+++ main.cpp
@@ -1,18 +1,11 @@
+#include <cstdlib>
+#include <ctime>
 #include "CImg.h"
 using namespace cimg_library;
 int main() {
-  CImg<unsigned char> image("lena.bmp"), visu(500,400,1,3,0);
-  const unsigned char red[] = { 255,0,0 }, green[] = { 0,255,0 }, blue[] = { 0,0,255 };
-  image.blur(2.5);
-  CImgDisplay main_disp(image,"Click a point"), draw_disp(visu,"Intensity profile");
-  while (!main_disp.is_closed() && !draw_disp.is_closed()) {
-    main_disp.wait();
-    if (main_disp.button() && main_disp.mouse_y()>=0) {
-      const int y = main_disp.mouse_y();
-      visu.fill(0).draw_graph(image.get_crop(0,y,0,0,image.width()-1,y,0,0),red,1,1,0,255,0);
-      visu.draw_graph(image.get_crop(0,y,0,1,image.width()-1,y,0,1),green,1,1,0,255,0);
-      visu.draw_graph(image.get_crop(0,y,0,2,image.width()-1,y,0,2),blue,1,1,0,255,0).display(draw_disp);
-      }
-    }
-  return 0;
+	std::srand(time(NULL));
+	CImg<uint8_t> img("lena.bmp");  // Define a 320x200 color image (3 channels).
+	
+	img.display("Display my image");
+	return 0;
 }
--- tes.cpp
+++ tes.cpp
@@ -1,29 +1,31 @@
 #include "CImg.h"
 #include <cstdio>
+#include <math.h>
 #define sizy 15
 using namespace cimg_library;
-unsigned char HSV(unsigned char r, unsigned char g, unsigned char b);
+unsigned char Hue(unsigned char r, unsigned char g, unsigned char b);
 int main() {
   CImg<unsigned char> base("color.bmp"), mask("layer.bmp");
-  CImg<unsigned char> sam(sizy,sizy,1,3); 
-  char i,j;
+  //CImg<unsigned char> sam(sizy,sizy,1,3); 
+  /*char i,j;
   for(i=0; i<sizy, i++){
     for(j=0; j<sizy;j++){
 
     }
-  }
-  sam = base.get_RGBtoHSL();
+  }*/
+  //Cimg<unsigned char> sam(base.width(), base.height())
+  //sam = base.get_RGBtoHSL();
   std::printf("width:%d, height:%d\n", base.width(), base.height());
   //base.blur(2.5);
-  CImgDisplay base_disp(base,"Base"), mask_disp(mask,"Mask"),sam_disp(sam,"Sam");
-  while (!base_disp.is_closed() && !mask_disp.is_closed() && !sam_disp.is_closed() ) {
+  CImgDisplay base_disp(base,"Base"), mask_disp(mask,"Mask");
+  while (!base_disp.is_closed() && !mask_disp.is_closed()  ) {
     base_disp.wait();
     mask_disp.wait();
-    sam_disp.wait();
+    //sam_disp.wait();
   }
   return 0;
 }
-unsigned char HSV(unsigned char r, unsigned char g, unsigned char b){
+/*unsigned char Hue(unsigned char r, unsigned char g, unsigned char b){
   unsigned char R,G,B;
   R=r/255
-}
+}*/
